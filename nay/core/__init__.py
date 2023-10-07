d57a8a9193e1059ae4d91167360c694eb72b1824
Zack Bunch <zackbunch96@gmail.com>
2023-10-07 06:54:57 -0700

    Added Core module to hold all CI/CD mappings

---
+++ nay/core/__init__.py
@@ -0,0 +1,6 @@
+"""This module will hold all [GitLab CI Keyword](https://docs.gitlab.com/ee/ci/yaml/)
+representations
+"""
+from typing import Dict
+
+OrderedSetType = Dict[str, None]
---
+++ nay/core/rules.py
@@ -0,0 +1,28 @@
+"""
+Model of GitLab CI [rules](https://docs.gitlab.com/ee/ci/yaml/#rules)
+"""
+from __future__ import annotations
+
+from enum import Enum
+
+
+class When(Enum):
+    """Holds different [when](https://docs.gitlab.com/ee/ci/yaml/#when)
+    statements for Rules
+
+    Args:
+        Enum (When): statements for Rules
+    """
+    ALWAYS = 'always'
+    DELAYED = 'delayed'
+    MANUAL = 'manual'
+    NEVER = 'never'
+    ON_FAILURE = 'on_failure'
+    ON_SUCCESS = 'on_success'
+
+class Rule:
+    """Represents a [rule](https://docs.gitlab.com/ee/ci/yaml/#rules)
+
+    Rules are used to include or exclude jobs in Pipelines
+    """
+    pass
